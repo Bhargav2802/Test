89d30165269047cf05884c14672ee22684844305
Bhargav2802 <156651629+Bhargav2802@users.noreply.github.com>
2024-11-21 22:04:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 for k in range(20):
     print(k + 1)
     
-    
 def add_function(n1 , n2):
     result(n1 + n2)
     print("The sum is", result)
@@ -10,3 +9,5 @@ number1 = 5
 number2 = 5
 
 add_number(number1 , number2)
+
+print input("Enter the Password": )
