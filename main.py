a967180c650e39fea49b38e1fffda2d6240d8bf7
Bhargav2802 <156651629+Bhargav2802@users.noreply.github.com>
2024-11-21 21:43:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 for k in range(20):
     print(k + 1)
+    
+    
+def add_function(n1 , n2):
+    result(n1 + n2)
+    print("The sum is", result)
+    
+number1 = 5
+number2 = 5
+
+add_number(number1 , number2)
